a9a1f9664921fa79ff252b773043642c24822698
rudyzhou2 <qian.q.zhou@mail.mcgill.ca>
2015-11-12 11:46:01 -0800

    Update htqc.rb
    Updated to htqc-0.90.8 and sha

--- htqc.rb
+++ htqc.rb
@@ -3,9 +3,11 @@ class Htqc < Formula
   #doi "10.1186/1471-2105-14-33"
   #tag "bioinformatics"
 
-  url "https://downloads.sourceforge.net/project/htqc/htqc-0.90.2-Source.tar.gz"
-  sha1 "c4204ed1b85d78daa3c90968b99eea113308ed72"
-
+  #url "https://downloads.sourceforge.net/project/htqc/htqc-0.90.2-Source.tar.gz"
+  #sha1 "c4204ed1b85d78daa3c90968b99eea113308ed72"
+  url "https://downloads.sourceforge.net/project/htqc/htqc-0.90.8-Source.tar.gz"
+  sha1 "7a0ff3c7e516077bc829bbf7fb48f92d0059fbcc"
+  
   bottle do
     cellar :any
     sha1 "fde8f0db51fa002e1b32fec422a74aaa120952f3" => :mountain_lion
