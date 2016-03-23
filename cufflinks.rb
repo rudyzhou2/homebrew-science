6ecd3b7c852070d61e21b70088381f792354e01a
rudyzhou2 <qian.q.zhou@mail.mcgill.ca>
2016-03-23 14:26:09 -0700

    Update cufflinks.rb

--- cufflinks.rb
+++ cufflinks.rb
@@ -1,16 +1,21 @@
 class Cufflinks < Formula
   homepage "http://cufflinks.cbcb.umd.edu/"
-  url "http://cole-trapnell-lab.github.io/cufflinks/assets/downloads/cufflinks-2.2.1.tar.gz"
+  url "https://cole-trapnell-lab.github.io/cufflinks/assets/downloads/cufflinks-2.2.1.tar.gz"
   sha256 "e8316b66177914f14b3a0c317e436d386a46c4c212ca1b2326f89f8a2e08d5ae"
 
   bottle do
-    cellar :any
-    sha256 "68750a99e5cf104666cfc068c4bfddf8c2aebe2cb44bf5360aac2b5f4e181000" => :yosemite
-    sha256 "3313084062765bee932d05d0bc8477887ec6a61e788c9a465c907cc363b44370" => :mavericks
-    sha256 "d1e789b72fe73af8cb46aa71feeedea398df2c4b1e9fce5604492a7945143487" => :mountain_lion
+    revision 1
+    sha256 "5fc3c25ec82ac08f4f6698293ae541314a276a0866c1c745ae4c621d92dfc357" => :el_capitan
+    sha256 "1877762ec375968e508f5ab07c586b50ab7032ca92ffc2661cca1a767572e855" => :yosemite
+    sha256 "5929896c71cdb5866f6cfb8da6d2c6b8a43ce4d4833529484aedef79a4a58ed3" => :mavericks
   end
 
-  depends_on "homebrew/versions/boost155" => :build
+  if OS.mac? && MacOS.version == :mavericks
+    depends_on "homebrew/versions/boost155"
+  else
+    depends_on "boost"
+  end
+
   depends_on "samtools-0.1" => :build
   depends_on "eigen"
 
